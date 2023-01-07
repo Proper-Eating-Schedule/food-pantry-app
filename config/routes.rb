f30f1d74d4d16b6801af9be0b7fd0ca6acaf17ec
oszegda <oszegda@icloud.com>
2023-01-07 17:33:25 +0100

    feat: FP-18 Added routes for pantry items CRUD paths

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users, path: '/'
   devise_for :admins, path: 'admins', controllers: { sessions: 'admins/sessions' }
+  resources :pantry_items
 
   namespace :admins do
     get '/', to: 'home#index'
