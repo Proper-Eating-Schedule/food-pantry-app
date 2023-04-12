f7339715ed097fdd42fc1f15bcd6a8ca26f8d7b0
oszegda <oszegda@icloud.com>
2023-04-13 00:38:01 +0200

    feat: FP-18 Updated tests with product id validation

--- spec/models/pantry_item_spec.rb
+++ spec/models/pantry_item_spec.rb
@@ -3,7 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe PantryItem, type: :model do
-  subject { PantryItem.new(name: 'TestPantryItem', quantity: 3, measurement: 'szt', expire_date: '2023-05-01') }
+  let!(:existing_category) { Category.create(name: 'Test4') }
+
+  let!(:existing_product) { Product.create(name: 'Test123', category_id: existing_category.id) }
+
+  subject do
+    PantryItem.new(name: 'TestPantryItem',
+                   quantity: 5, measurement: 'szt', expire_date: '25-04-2023', product_id: existing_product.id)
+  end
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
@@ -26,6 +33,11 @@ RSpec.describe PantryItem, type: :model do
 
   it 'is not valid without a expire date' do
     subject.expire_date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a product id' do
+    subject.product_id = nil
     expect(subject).to_not be_valid
   end
 end
